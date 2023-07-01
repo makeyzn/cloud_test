f5e31d85ead0423c1045900c0adbf594153252cc
makeyzn <tntmr774@gmail.com>
2023-07-01 23:56:15 +0300

    feat: phone number mask

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -2,9 +2,11 @@ import "../App.css";
 import { useNavigate } from "react-router-dom";
 import FolderIcon from "@mui/icons-material/Folder";
 import { useForm } from "react-hook-form";
+import InputMask from 'react-input-mask';
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import style from "../modules/mainPage.module.css";
+import button from "../modules/Button.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { addMain } from "../features/Advantages/Data-slice";
 
@@ -17,13 +19,11 @@ const mainPage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const data = useSelector((state) => state.data);
-  const phoneRegExp = /^[0-9]{10}$/;
 
   const schema = yup.object().shape({
     phone: yup
       .string()
-      .required()
-      .matches(phoneRegExp, "Phone number is not valid"),
+      .required(),
     email: yup.string().email().required(),
   });
 
@@ -42,25 +42,25 @@ const mainPage = () => {
     dispatch(addMain(data));
     navigate("/create/step1");
   };
-  
+
   return (
     <>
       {/* Семантика? */}
       <div className={style.container}>
         <div className={style.main}>
-          <div className={style.main__logo}>ДИ</div>
+          <div className={style.main__logo}>ВК</div>
           <div className={style.main__contacts}>
-            <h3>Иван Иванов</h3>
+            <h3>Владимир Конаков</h3>
             <ul className={style.main__contacts_links}>
               {/* Убрать HARDCODE STYLE */}
               {/* Рассмотреть вариант замены 'li' n 'a' */}
               <li>
                 <FolderIcon style={{ color: "#CCCCCC", marginRight: "5px" }} />
-                <a href="#">Telegram</a>
+                <a href="https://t.me/makeyzn">Telegram</a>
               </li>
               <li>
                 <FolderIcon style={{ color: "#CCCCCC", marginRight: "5px" }} />
-                <a href="#">GitHub</a>
+                <a href="https://github.com/makeyzn">GitHub</a>
               </li>
               <li>
                 <FolderIcon style={{ color: "#CCCCCC", marginRight: "5px" }} />
@@ -72,23 +72,23 @@ const mainPage = () => {
         <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
           <div className={style.form__container}>
             <p>Номер телефона</p>
-            <input
+            <InputMask 
+              mask='+7 (999) 999-99-99' 
               type="tel"
-              placeholder="+7 999"
+              placeholder="+7 (___) ___-__-__"
               {...register("phone")}
-              className={style.form__input}
-            />
+              className={style.form__input}></InputMask>
             <p>{errors.phone?.message}</p>
             <p>Email</p>
             <input
               type="text"
-              placeholder="tntmr774@gmail.com"
+              placeholder="konakovv66@yandex.ru"
               {...register("email")}
               className={style.form__input}
             />
             <p>{errors.email?.message}</p>
           </div>
-          <button id="button-start" className="buttonNext" type="submit" >
+          <button id="button-start" className={button.buttonNext} type="submit" >
             Начать
           </button>
         </form>
