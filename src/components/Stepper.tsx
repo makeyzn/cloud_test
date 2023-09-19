a1dcdc595454ff9228c5785718b17725cdf95b2f
makeyzn <tntmr774@gmail.com>
2023-09-19 16:36:07 +0300

    refactor: Refactor Stepper component

--- src/components/Stepper.tsx
+++ src/components/Stepper.tsx
@@ -1,61 +1,63 @@
-import { styled } from '@mui/material/styles';
-import Stack from '@mui/material/Stack';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Check from '@mui/icons-material/Check';
-import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector';
-import { StepIconProps } from '@mui/material/StepIcon';
+import { styled } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Check from "@mui/icons-material/Check";
+import StepConnector, {
+  stepConnectorClasses,
+} from "@mui/material/StepConnector";
+import { StepIconProps } from "@mui/material/StepIcon";
 
 const QontoConnector = styled(StepConnector)(({ theme }) => ({
   [`&.${stepConnectorClasses.alternativeLabel}`]: {
     top: 6,
-    left: 'calc(-50%)',
-    right: 'calc(50%)',
+    left: "-50%",
+    right: "50%",
   },
   [`&.${stepConnectorClasses.active}`]: {
     [`& .${stepConnectorClasses.line}`]: {
-      borderColor: '#5558FA',
-      
+      borderColor: "#5558FA",
     },
   },
   [`&.${stepConnectorClasses.completed}`]: {
     [`& .${stepConnectorClasses.line}`]: {
-      borderColor: '#5558FA',
+      borderColor: "#5558FA",
     },
   },
   [`& .${stepConnectorClasses.line}`]: {
-    borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+    borderColor:
+      theme.palette.mode === "dark" ? theme.palette.grey[800] : "#eaeaf0",
     borderTopWidth: 8,
     borderRadius: 1,
   },
 }));
 
-const QontoStepIconRoot = styled('div')<{ ownerState: { active?: boolean } }>(
+const QontoStepIconRoot = styled("div")<{ ownerState: { active?: boolean } }>(
   ({ theme, ownerState }) => ({
-    color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#A6A6A6',
-    display: 'flex',
+    color: theme.palette.mode === "dark" ? theme.palette.grey[700] : "#A6A6A6",
+    display: "flex",
     height: 22,
-    alignItems: 'center',
+    alignItems: "center",
     ...(ownerState.active && {
-      color: '#5558FA',
+      color: "#5558FA",
     }),
-    '& .QontoStepIcon-completedIcon': {
-      color: '#5558FA',
+    "& .QontoStepIcon-completedIcon": {
+      color: "#5558FA",
       zIndex: 1,
       fontSize: 16,
       padding: 1,
-      fontWeight: 'normal'
+      fontWeight: "normal",
     },
-    '& .QontoStepIcon-circle': {
+    "& .QontoStepIcon-circle": {
       width: 16,
       height: 16,
-      padding: '5px',
-      borderRadius: '50%',
-      backgroundColor: 'currentColor',
+      padding: "5px",
+      borderRadius: "50%",
+      backgroundColor: "currentColor",
       zIndex: 1,
     },
-  }),
+  })
 );
 
 function QontoStepIcon(props: StepIconProps) {
@@ -64,7 +66,14 @@ function QontoStepIcon(props: StepIconProps) {
   return (
     <QontoStepIconRoot ownerState={{ active }} className={className}>
       {completed ? (
-        <Check className="QontoStepIcon-completedIcon" style={{backgroundColor: '#5558FA', color: '#fff', borderRadius: '100%'}}/>
+        <Check
+          className="QontoStepIcon-completedIcon"
+          style={{
+            backgroundColor: "#5558FA",
+            color: "#fff",
+            borderRadius: "100%",
+          }}
+        />
       ) : (
         <div className="QontoStepIcon-circle" />
       )}
@@ -72,16 +81,19 @@ function QontoStepIcon(props: StepIconProps) {
   );
 }
 
-
-const steps = ['1', '2', '3'];
+const steps = ["1", "2", "3"];
 
 export default function CustomizedSteppers(props: any) {
   return (
-    <Stack sx={{ width: '1000px' }} spacing={4}>
-      <Stepper alternativeLabel activeStep={props.activeStep} connector={<QontoConnector />}>
+    <Stack width={1000} spacing={4}>
+      <Stepper
+        alternativeLabel
+        activeStep={props.activeStep}
+        connector={<QontoConnector />}
+      >
         {steps.map((label) => (
-          <Step key={label} >
-            <StepLabel StepIconComponent={QontoStepIcon} >{label}</StepLabel>
+          <Step key={label}>
+            <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
           </Step>
         ))}
       </Stepper>
